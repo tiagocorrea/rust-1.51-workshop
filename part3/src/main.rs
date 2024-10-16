d51f75317d8a367752a06110d7d2edc3fb972391
Tiago Corrêa <developer@tiagocorrea.me>
2024-10-16 14:21:33 -0300

    Part 3: Pattern Matching

--- part3/src/main.rs
+++ part3/src/main.rs
@@ -21,16 +21,18 @@ impl City {
                     residents,
                 )
             }
-            // 👉 TODO Handle the other CitySize variants individually,
-            //    in a similar way to how *town* is handled here
-            _ => {
-                let residents = 1_000;
+            CitySize::City => {
+                let residents = 10_000;
 
                 (
-                    format!(
-                        "an *unknown-size city* of approximately {} residents",
-                        residents
-                    ),
+                    format!("a *city* of approximately {} residents", residents),
+                    residents,
+                )
+            }
+            CitySize::Metropolis => {
+                let residents = 1_000_000;
+                (
+                    format!("a *metropolis* of approximately {} residents", residents),
                     residents,
                 )
             }
@@ -45,15 +47,44 @@ impl City {
 }
 
 fn main() {
-    // 👉 TODO Use City::new() to create a Metropolis-sized city here
-    let rustville = City {
-        description: String::new(),
-        residents: 0,
-    };
+    for city_size in [CitySize::Town, CitySize::City, CitySize::Metropolis] {
+        let city = City::new(city_size, true);
+        println!("This city is {}", city.description);
 
-    println!("This city is {}", rustville.description);
+        if city.residents > 100_000 {
+            println!("Wow!");
+        }
+    }
+}
 
-    if rustville.residents > 100_000 {
-        println!("Wow!");
+#[cfg(test)]
+mod tests {
+    use crate::{City, CitySize};
+
+    #[test]
+    fn test_city_new() {
+        let cases = [
+            (
+                CitySize::Town,
+                true,
+                "a *town* of approximately 1000 residents",
+            ),
+            (
+                CitySize::City,
+                false,
+                "a *city* of approximately 10000 residents",
+            ),
+            (
+                CitySize::Metropolis,
+                true,
+                "a *metropolis* of approximately 1000000 residents",
+            ),
+        ];
+        for (city_size, is_coastal, description) in cases {
+            let city = City::new(city_size, is_coastal);
+
+            assert_eq!(city.description, description);
+            assert_eq!(city.is_coastal, is_coastal);
+        }
     }
 }
